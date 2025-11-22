86f3d958080e55628a9fea2450a8c31967b25708
Jaarda9 <sk1lldr1llxno@gmail.com>
2025-11-22 12:33:05 +0100

    d

--- src/pages/Analytics.tsx
+++ src/pages/Analytics.tsx
@@ -32,7 +32,7 @@ const Analytics = () => {
           <Button
             variant="ghost"
             size="sm"
-            onClick={() => navigate('/dashboard')}
+            onClick={() => navigate('/')}
             className="mb-2 font-mono-data"
           >
             <ArrowLeft className="h-4 w-4 mr-1" />
